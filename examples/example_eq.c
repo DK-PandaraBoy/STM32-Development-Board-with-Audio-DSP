9b08e5c48831353ed70e42fa05bbb72c2a1cf816
DK-PandaraBoy <panda8dk.de@gmail.com>
2025-10-06 11:43:25 +0200

    Update example_eq.c

--- examples/example_eq.c
+++ examples/example_eq.c
@@ -1,1 +1,8 @@
+#include "audio_processing.h"
 
+
+void run_eq_demo(void) {
+int16_t input[AUDIO_BUFFER_SIZE];
+int16_t output[AUDIO_BUFFER_SIZE];
+Audio_ProcessBlock(input, output, AUDIO_BUFFER_SIZE);
+}
