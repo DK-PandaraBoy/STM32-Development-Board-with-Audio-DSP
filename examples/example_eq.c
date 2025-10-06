329b22fe64c63b91041428b83a8e2e5886371bcc
DK-PandaraBoy <panda8dk.de@gmail.com>
2025-10-06 11:52:42 +0200

    Update example_eq.c

--- examples/example_eq.c
+++ examples/example_eq.c
@@ -1,8 +1,27 @@
-#include "audio_processing.h"
+#include "dsp_filters.h"
+#include "arm_math.h"
 
 
-void run_eq_demo(void) {
-int16_t input[AUDIO_BUFFER_SIZE];
-int16_t output[AUDIO_BUFFER_SIZE];
-Audio_ProcessBlock(input, output, AUDIO_BUFFER_SIZE);
+#define BLOCK_SIZE 128
+#define NUM_TAPS 64
+
+
+float32_t inputBuffer[BLOCK_SIZE];
+float32_t outputBuffer[BLOCK_SIZE];
+float32_t firState[BLOCK_SIZE + NUM_TAPS - 1];
+float32_t firCoeffs[NUM_TAPS] = { /* band-pass filter coeffs */ };
+
+
+arm_fir_instance_f32 S;
+
+
+void eq_filter_demo(void) {
+arm_fir_init_f32(&S, NUM_TAPS, firCoeffs, firState, BLOCK_SIZE);
+
+
+while (1) {
+getAudioBlock(inputBuffer, BLOCK_SIZE);
+arm_fir_f32(&S, inputBuffer, outputBuffer, BLOCK_SIZE);
+sendAudioBlock(outputBuffer, BLOCK_SIZE);
+}
 }
